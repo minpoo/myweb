3872f97623b51988f67a8fee07333bc0f7cd7ad5
XIONG ZHONGBO <mindotchina@gmail.com>
2019-04-10 10:59:35 +0800

    no message

---
+++ my/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+		<view>
+            <text class="title">{{title}}</text>
+			<a href="http://www.cxcuu.com">欢迎</a>
+        </view>
+	</view>
+</template>
+
+<script>
+	//window.location.href="http://www.cxcuu.com"
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+            // window.location.href="http://www.cxcuu.com"
+		},
+		methods: {
+               
+		}
+	}
+	
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
---
+++ my/unpackage/dist/dev/.tmp/app-plus/0.js
@@ -0,0 +1,27 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([[0],{
+
+/***/ "../../../../../../Users/a1/myweb/my/main.js?{\"page\":\"\"}":
+/*!**********************************************!*\
+  !*** /Users/a1/myweb/my/main.js?{"page":""} ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__(/*! uni-pages */ \"../../../../../../Users/a1/myweb/my/pages.json\");\n\n//# sourceURL=Users/a1/myweb/my/main.js?%7B%22page%22:%22%22%7D");
+
+/***/ }),
+
+/***/ "../../../../../../Users/a1/myweb/my/pages.json":
+/*!*************************************!*\
+  !*** /Users/a1/myweb/my/pages.json ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n//# sourceURL=Users/a1/myweb/my/pages.json");
+
+/***/ })
+
+},[["../../../../../../Users/a1/myweb/my/main.js?{\"page\":\"\"}","common/runtime"]]]);
